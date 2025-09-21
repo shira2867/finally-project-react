00b0804708ab12ff1bdc222476d7db31d98662ab
shira goldstein <shiragoldstein2867@gmail.com>
2025-09-21 15:48:57 +0300

    level4

--- src/services/storeService.tsx
+++ src/services/storeService.tsx
@@ -1,7 +1,6 @@
-export const getProducts = () => {
-  return fetch("https://fakestoreapi.com/products")
-    .then(response => {
-      if (!response.ok) throw new Error("Network response was not ok");
-      return response.json();
-    });
+export const getProducts =async () => {
+  const response=await fetch("https://fakestoreapi.com/products");
+  const data=await response.json;
+
+      return data;
 };
